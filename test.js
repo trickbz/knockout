60371aeac2db01b64f3bae2fb99b8e26375e873c
Alexey Petrenko <apetrenko@lohika.com>
2015-08-14 22:31:34 +0300

    Slickgrid initial demo

--- test.js
+++ test.js
@@ -74,37 +74,73 @@ $(function () {
 		data: availableTags,
 		placeholder: "Select a state..."
 	});
-	
+
 	var dataTokenInput = [
-		{ id: 1, name: "ActionScript"},
-		{ id: 2, name: "AppleScript"},
-		{ id: 3, name: "Asp"},
-		{ id: 4, name: "BASIC"},
-		{ id: 5, name: "C"},
-		{ id: 6, name: "C++"},
-		{ id: 7, name: "Clojure"},
-		{ id: 8, name: "COBOL"},
-		{ id: 9, name: "ColdFusion"},
-		{ id: 10, name: "Erlang"},
-		{ id: 11, name: "Fortran"},
-		{ id: 12, name: "Groovy"},
-		{ id: 13, name: "Haskell"},
-		{ id: 14, name: "Java"},
-		{ id: 15, name: "JavaScript"},
-		{ id: 16, name: "Lisp"},
-		{ id: 17, name: "Perl"},
-		{ id: 18, name: "PHP"},
-		{ id: 19, name: "Python"},
-		{ id: 20, name: "Ruby"},
-		{ id: 21, name: "Scala"},
-		{ id: 22, name: "Scheme"}
+		{ id: 1, name: "ActionScript" },
+		{ id: 2, name: "AppleScript" },
+		{ id: 3, name: "Asp" },
+		{ id: 4, name: "BASIC" },
+		{ id: 5, name: "C" },
+		{ id: 6, name: "C++" },
+		{ id: 7, name: "Clojure" },
+		{ id: 8, name: "COBOL" },
+		{ id: 9, name: "ColdFusion" },
+		{ id: 10, name: "Erlang" },
+		{ id: 11, name: "Fortran" },
+		{ id: 12, name: "Groovy" },
+		{ id: 13, name: "Haskell" },
+		{ id: 14, name: "Java" },
+		{ id: 15, name: "JavaScript" },
+		{ id: 16, name: "Lisp" },
+		{ id: 17, name: "Perl" },
+		{ id: 18, name: "PHP" },
+		{ id: 19, name: "Python" },
+		{ id: 20, name: "Ruby" },
+		{ id: 21, name: "Scala" },
+		{ id: 22, name: "Scheme" }
 	];
-	
+
 	$('#tokeninput').tokenInput(dataTokenInput, {
 		hintText: "Choose a technology",
-		theme: "facebook",
-		tokenDelimiter: ";"
+		theme: "facebook"
 	});
-	
+
 	$("#spinner").spinner().spinner("value", 5);
+	
+	// slick grid
+	var linkFormatter = function (row, cell, value, columnDef, dataContext) {
+        return "<a href='" + value.href + "' tabindex='0'>" + value.text + "</a>";
+    };
+
+	var grid;
+	var columns = [
+		{ id: "url", name: "URL", field: "url", formatter: linkFormatter },
+		// { id: "duration", name: "Duration", field: "duration" },
+		// { id: "%", name: "% Complete", field: "percentComplete" },
+		// { id: "start", name: "Start", field: "start" },
+		// { id: "finish", name: "Finish", field: "finish" },
+		// { id: "effort-driven", name: "Effort Driven", field: "effortDriven" }
+	];
+	var options = {
+		enableCellNavigation: true,
+		enableColumnReorder: false
+	};
+
+	var gridData = [
+		{ url: { href: "http://www.google.com", text: "Google" } }
+	];
+
+	// var data = [];
+	// for (var i = 0; i < 500; i++) {
+	// 	data[i] = {
+	// 		title: "<a href='" + i + "' tabindex='0'>Task " + i + "</a>",
+	// 		// duration: "5 days",
+	// 		// percentComplete: Math.round(Math.random() * 100),
+	// 		// start: "01/01/2009",
+	// 		// finish: "01/05/2009",
+	// 		// effortDriven: (i % 5 == 0)
+	// 	};
+	// }
+	grid = new Slick.Grid("#slickGrid", gridData, columns, options);
+
 });
