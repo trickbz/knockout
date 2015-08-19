380d9f02749e46fb3e3f89cb1efe3eb22adf842b
Alexey Petrenko <apetrenko@lohika.com>
2015-08-20 00:50:39 +0300

    tokenInput not final

--- js/bindings.js
+++ js/bindings.js
@@ -20,10 +20,42 @@ ko.bindingHandlers.tokenInput = {
 	init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
 		var $el = $(element);
 		var options = allBindings().tokenInputOptions || {};
-		var observable = valueAccessor();
-		$el.tokenInput(observable(), options);
+		var self = this;
+		this.tokens = valueAccessor();
+		element.isUpdating = false;
+		
+		$el.tokenInput(options.url, $.extend(options, {
+			onAdd: function(item) {
+				// if (!element.isUpdating) {
+					element.isUpdating = true;
+					self.tokens.push(item);					
+					element.isUpdating = false;
+				// }
+			},
+			onDelete: function(item) {
+				// if (!element.isUpdating) {
+					element.isUpdating = true;
+					self.tokens.remove(item);
+					element.isUpdating = false;	
+				// }
+			},
+			addAll: function() {
+				self.tokens.forEach(function(token) {
+					$(element).tokenInput('add', token);
+				}, this);
+			}
+		}));
 	},
 	update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
-		
+		var tokens = ko.toJS(valueAccessor());
+		var $el = $(element);
+		// if (element.isUpdating) {
+			element.isUpdating = true;
+			$el.tokenInput('clear');
+			for (var i = 0; i < tokens.length; i++) {
+				$el.tokenInput('add', tokens[i]);
+			}
+			element.isUpdating = false;
+		// }
 	}
 };
